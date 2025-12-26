2855e357ac21c3d2dd0f444f59b5b0b3e81cfa9d
kimzooeun <asdo6863@naver.com>
2025-12-26 15:15:28 +0900

    Revert "[FE] feature: 헤더 메뉴 변경"
    This reverts commit 87a3b5d953e38f45890b11dca2d60245e4b73a4c.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,10 +7,6 @@ export default function Header() {
     navigate("/");
   };
 
-  const handlePlanClick = () => {
-    navigate("/mytrips"); // My Plan 클릭 시 바로 이동
-  };
-
   const handleLoginClick = () => {
     // 원본 동작 없음 (유지)
   };
@@ -23,18 +19,6 @@ export default function Header() {
 
       <nav>
         <ul>
-          {/* 순서 변경 1: MY PLAN (토글 제거) */}
-          <li className="nav-group">
-            <button
-              className="nav-item"
-              type="button"
-              onClick={handlePlanClick}
-            >
-              MY PLAN ❯
-            </button>
-          </li>
-
-          {/* 순서 변경 2: COMMUNITY (드롭다운 유지) */}
           <li className="nav-group">
             <button className="nav-item" type="button">
               COMMUNITY ▼
@@ -42,6 +26,15 @@ export default function Header() {
             <div className="dropdown-menu">
               <a href="/community">{">>"} DATA LOGS</a>
               <a href="/mate">{">>"} MATE FINDER</a>
+            </div>
+          </li>
+
+          <li className="nav-group">
+            <button className="nav-item" type="button">
+              MY PLAN ▼
+            </button>
+            <div className="dropdown-menu">
+              <a href="/mytrips">{">>"} ALL</a>
             </div>
           </li>
         </ul>
